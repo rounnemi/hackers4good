a4421c4ef681ca91f50f71f7d2bff4f7bfacda03
rounnemi <nour.soussia@insat.ucar.tn>
2023-11-12 22:49:54 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Aouinet",
       home: Scaffold(
         body: MyBackground(),
       ),
