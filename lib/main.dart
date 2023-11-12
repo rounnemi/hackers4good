93248802dbfcd2d46f3bb5fed23c42ad571a3bf1
rounnemi <nour.soussia@insat.ucar.tn>
2023-11-12 22:28:36 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: MyBackground(),
+      ),
+    );
+  }
+}
+
+class MyBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image1.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        MyContent(),
+      ],
+    );
+  }
+}
+
+class MyContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 3,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Image.asset(
+              'assets/image2.png',
+              width: 200.0,
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'Aouinet',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              // Gérez la navigation vers la page de connexion ici
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              textStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+            child: Text(
+              'Aouinet',
+              style: TextStyle(
+                color: Colors.cyanAccent,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                backgroundColor: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BackgroundLogin(),
+    );
+  }
+}
+
+
+class BackgroundLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image3.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Log In',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            // Votre widget de connexion ici
+            login(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+class login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.5, // 50% de la largeur de l'écran
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'User Name',
+                  filled: true, // Activez le remplissage du champ de texte
+                  fillColor: Colors.white, // Couleur d'arrière-plan blanche
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true, // Pour masquer le mot de passe
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true, // Activez le remplissage du champ de texte
+                  fillColor: Colors.white, // Couleur d'arrière-plan blanche
+                ),
+              ),
+              SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Gérez l'action de connexion ici
+                  // Vous pouvez ajouter ici la logique pour vérifier les informations de connexion
+                },
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    color: Colors.cyanAccent,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
